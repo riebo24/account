5e1644e88a3a78ee6923b4c54bd8f04673ff0e33
rie1224 <a10ea211@yahoo.co.jp>
2020-01-16 17:11:14 +0900

    set edit,update,destroy, index action
    全てのテーブルにedit,update,destroy,indexアクション、ビューを設定。

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def index
-    
+    @categories = Category.all
   end
 
   def new
@@ -12,31 +12,31 @@ class CategoriesController < ApplicationController
     @category = Category.new(set_category)
     @category[:budget_id] = 1
     @category[:user_id] = 1
-    binding.pry
-
     if @category.save
-
       redirect_to root_path
-
     else
-
       render :new
     end
   end
 
   def edit
     @category = Category.find(1)
-
   end
 
-  def update   
+  def update
+    @category = Category.find(params[:id])   
+    @category.update(set_category)
+    redirect_to categories_path
   end
 
-
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path    
+  end
 
   private
     def set_category
-
       params.require(:category).permit(:name)
     end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
-    
+    @tags = Tag.all
   end
 
   def new
@@ -17,13 +17,20 @@ class TagsController < ApplicationController
     end
   end
 
-
   def edit
-    
+    @tag = Tag.find(params[:id])    
   end
 
   def update
-    
+    @tag = Tag.find(params[:id])   
+    @tag.update(set_tag)
+    redirect_to tags_path
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+    redirect_to tags_path    
   end
 
   private
