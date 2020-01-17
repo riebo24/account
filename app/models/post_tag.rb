08e67fde0560eb4708815d526ad2f57ea531825b
rie1224 <a10ea211@yahoo.co.jp>
2020-01-17 19:37:22 +0900

    edit monthly form
    月予算のフォームで予算を編集できるよう編集しました。

--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,5 +1,5 @@
 class PostTag < ApplicationRecord
-  belongs_to :posts
-  belongs_to :tags
-  belongs_to :user
+  belongs_to :posts, optional: true
+  belongs_to :tags, optional: true
+  belongs_to :user, optional: true
 end
