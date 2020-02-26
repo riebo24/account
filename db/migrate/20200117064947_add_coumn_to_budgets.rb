a3249eb8d13f11aefd9ef21520c8e028dbef57c2
rie1224 <a10ea211@yahoo.co.jp>
2020-02-26 12:52:28 +0900

    マイグレーションファイルの変更

--- db/migrate/20200117064947_add_coumn_to_budgets.rb
+++ db/migrate/20200117064947_add_coumn_to_budgets.rb
@@ -1,6 +1,5 @@
 class AddCoumnToBudgets < ActiveRecord::Migration[5.0]
   def change
-    remove_reference :budgets, :monthly_budget, foreign_key: true
     add_reference :budgets, :monthly, foreign_key: true
   end
 end
