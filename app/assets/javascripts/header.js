dabc6466a862e94b64ef0c26395793457be22287
rie1224 <a10ea211@yahoo.co.jp>
2020-02-27 14:27:08 +0900

    ハンバーガーメニューのアイコン追加

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,6 +1,5 @@
 $(function(){
 
-
   $(".detailed_header").on("click", function(){
     $(".menu").toggleClass('open-menu');
     $(".posts").css({"width":"calc(100% - 300px)", "margin-left":"200px", "transition": ".7s ease-in-out"})
@@ -8,15 +7,11 @@ $(function(){
   })
 
   $(document).on("click", ".close-menu", function(){
-    console.log("発火")
     $(".posts").css({"width":"100%", "margin-left":"0px", "transition": ".7s ease-in-out"})
   })
 
   $(document).on("click", ".menu", function(){
-    console.log("発火")
     $(".posts").css({"width":"100%", "margin-left":"0px", "transition": ".7s ease-in-out", })
     $(".index").css({"height":"100%", "width":"100%" , "opacity": "1", "transition": ".7s ease-in-out"})
   })
-
-
 })
