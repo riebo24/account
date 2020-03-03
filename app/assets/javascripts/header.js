51ee783c2b07d7b9ef71fac9c3e5106f9327f24a
rie1224 <a10ea211@yahoo.co.jp>
2020-03-03 11:35:24 +0900

    ユーザーのログアウト機能の実装

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -2,21 +2,22 @@ $(function(){
 
   $(".detailed_header").on("click", function(){
     $(".menu").toggleClass('open-menu');
-    $(".content").css({"width":"calc(100% - 300px)", "margin-left":"200px", "opacity": "0.6", "transition": ".7s ease-in-out"})
-    // $(".index").css({"height":"100%", "width":"100%",  "transition": ".7s ease-in-out"})
+    $(".content").css({"width":"calc(100% - 300px)", "margin-left":"200px", "opacity": "0.5", "transition": ".7s ease-in-out"})
+    $(".post").css({"width":"calc(100% - 300px)", "margin-left":"200px", "opacity": "0.5", "transition": ".7s ease-in-out"})
   })
 
+
   $(document).on("click", ".close-menu", function(){
-    $(".content").css({"width":"100%", "margin-left":"0px", "transition": ".7s ease-in-out"})
+    $(".content").css({"width":"100%", "margin-left":"0px", "opacity": "1","transition": ".7s ease-in-out"})
+    $(".post").css({"width":"90%", "margin-left":"0px", "opacity": "1","transition": ".7s ease-in-out"})
   })
 
   $(document).on("click", ".menu", function(){
-    $(".content").css({"width":"100%", "margin-left":"0px", "opacity": "1","transition": ".7s ease-in-out", })
-    // $(".index").css({"height":"100%", "width":"100%" ,  "transition": ".7s ease-in-out"})
+    $(".content").css({"width":"100%", "margin-left":"0px", "opacity": "1","transition": ".7s ease-in-out"})
+    $("post").css({"width":"90%", "margin-left":"0px", "opacity": "1","transition": ".7s ease-in-out"})
   })
 
   $(".humberger_icon").on({"mouseenter": function(){
-    console.log("発火")
     $("span#humberger").css({"background":"black"})},
     
     'mouseleave': function(){
