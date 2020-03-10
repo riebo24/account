8fdbf0d88914615e651ebe7181a03e1f370b8447
rie1224 <a10ea211@yahoo.co.jp>
2020-03-10 16:17:29 +0900

    Postモデル単体テスト
    ・createアクション
    ・updateアクション

---
+++ spec/factories/post.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :post do
+    p_price {"1298"}
+    memo {"今週末のパーティー用にスーパーでお肉とじゃがいもを買いました"}
+    date {"2020-03-01"}
+    user
+    category
+    created_at { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now)}
+  end
+end
