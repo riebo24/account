e8d880812c26fa46cc43dfa96f0457e2183b1d64
rie1224 <a10ea211@yahoo.co.jp>
2020-03-10 16:14:12 +0900

    Userモデル
    Userモデル単体テスト
    ・updateアクションテスト
    ・Fakerを利用して、ダミーデータを作成

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   # テストで使用するインスタンス（テストをパスする）をあらかじめ作成
   factory :user do
     name              {"テスト太郎"}
-    email                 {"aaa@gmail.com"}
+    # gem[Faker]を利用。複数のインスタンスを作成する際、異なるメールアドレスでuserを作成できる。
+    sequence(:email) {Faker::Internet.email}
     password              {"11111111"}
     password_confirmation {"11111111"}
   end
